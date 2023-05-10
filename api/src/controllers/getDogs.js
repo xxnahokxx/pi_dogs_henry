1d20e3c5f73f779a30281514760cd34f1e0cb84f
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-05-10 00:02:45 -0500

    se generan los controladores de getDogs por Query getRazaId y getDogs

--- api/src/controllers/getDogs.js
+++ api/src/controllers/getDogs.js
@@ -4,21 +4,45 @@ const { END_POINT, API_KEY } = process.env;
 
 
 const getDogs = async (req, res) => {
+    const { name } = req.query;
+    console.log(req.query);
     try {
-        const { data } = await axios.get(`${END_POINT}`)
-        const info = data.map(el => {
-            return {
-                id: el.id,
-                image: el.image.url,
-                name: el.name,
-                temperament: el.temperament,
-                weight: el.weight.metric,
-                height: el.height.metric,
-            }
-        });
-        console.log(info);
-        res.status(200).json(info)
+        if (name) {
+            const { data } = await axios.get(`${END_POINT}/search?q=${name}&api_key=${API_KEY}`)
+            console.log("esto es data ====", data);
+            const info = data.map(el => {
+                return {
+                    id: el.id,
+                    image: el.reference_image_id,
+                    name: el.name,
+                    temperament: el.temperament,
+                    weight: el.weight.metric,
+                    height: el.height.metric,
+                    life_span: el.life_span,
+                }
+            });
 
+            console.log(info);
+            res.status(200).json(info)
+
+        } else {
+            const { data } = await axios.get(`${END_POINT}`)
+            const info = data.map(el => {
+                return {
+                    id: el.id,
+                    image: el.image.url,
+                    name: el.name,
+                    temperament: el.temperament,
+                    weight: el.weight.metric,
+                    height: el.height.metric,
+                    life_span: el.life_span,
+                }
+            });
+
+            console.log(info);
+            res.status(200).json(info)
+
+        }
 
     } catch (error) {
         res.status(500).json({ error: error.message });
--- api/src/controllers/getRazaId.js
+++ api/src/controllers/getRazaId.js
@@ -1,6 +1,30 @@
-const getRazaId = async (req, res) => {
-    try {
+const axios = require('axios');
+require("dotenv").config();
+const { END_POINT, API_KEY } = process.env;
 
+const getRazaId = async (req, res) => {
+    const { idRaza } = req.params;
+    try {
+        const { data } = await axios.get(`${END_POINT}`)
+        const info = data.find(el => el.id === Number(idRaza));
+        const { id,
+            reference_image_id,
+            name,
+            temperament,
+            weight: { metric: weight },
+            height: { metric: height },
+            life_span,
+        } = info;
+        console.log(height);
+        res.status(200).json({
+            id,
+            reference_image_id,
+            name,
+            temperament,
+            weight,
+            height,
+            life_span,
+        });
     } catch (error) {
 
     }
