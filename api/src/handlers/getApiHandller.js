78930518ef0a3e95975250d3d398fb4878a10be8
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-05-16 13:42:55 -0500

    se crea el handler que trae la info de la API

---
+++ api/src/handlers/getApiHandller.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+require("dotenv").config();
+const { END_POINT, API_KEY } = process.env;
+
+const getApiHandller = async (search) => {
+
+        const { data } = await axios.get(`${END_POINT}/search?q=${search}&api_key=${API_KEY}`)
+        const info = data.map(el => {
+            return {
+                id: el.id,
+                image: el.reference_image_id,
+                name: el.name,
+                temperament: el.temperament,
+                weight: el.weight.metric,
+                height: el.height.metric,
+                life_span: el.life_span,
+            }
+        });
+
+        return info;
+
+}
+
+
+module.exports = getApiHandller;
