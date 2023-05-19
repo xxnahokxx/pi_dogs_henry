77e3669413382448b4f4713be8d4017633b7c180
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-05-19 03:22:30 -0500

    se crea el componente del formulario de creacion de perros

---
+++ client/src/components/formCreate/FormCreate.jsx
@@ -0,0 +1,9 @@
+import style from "./formCreate.module.css";
+
+const FormCreate = () => {
+    return (
+        <div>estas en el formulario de creacion</div>
+    )
+}
+
+export default FormCreate;
