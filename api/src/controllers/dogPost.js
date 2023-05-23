c5e276f0d6d01369d75cf06a31bbfab40cb01752
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-05-23 11:58:48 -0500

    se hacen correcciones en las solicitudes.

--- api/src/controllers/dogPost.js
+++ api/src/controllers/dogPost.js
@@ -2,21 +2,24 @@ const countDbHandler = require("../handlers/countDbHandler");
 const { dogPostHandlers } = require("../handlers/index")
 
 const dogPost = async (req, res) => {
-    const { name, heigth, weight, life_span, temperament, image } = req.body;
+    const { name, height, weight, life_span, temperament, image } = req.body;
+
+    console.log(name);
+
     try {
 
-        if (!name || !heigth || !weight || !life_span || !temperament) res.status(401).json({ message: "faltan datos" });
+        if (!name || !height || !weight || !life_span || !temperament) return res.status(401).json({ message: "faltan datos" });
 
         const conteo = await countDbHandler();
 
         console.log(conteo);
 
-        const data = await dogPostHandlers(name, heigth, weight, life_span, temperament, conteo, image);
+        const data = await dogPostHandlers(name, height, weight, life_span, temperament, conteo, image);
 
-        res.status(200).json(data);
+        return res.status(200).json(data);
 
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        return res.status(500).json({ error: error.message });
     }
 };
 
--- api/src/handlers/getIdDbHandler.js
+++ api/src/handlers/getIdDbHandler.js
@@ -1,11 +1,32 @@
-const { Dog } = require("../db");
+const { Dog, Temperament } = require("../db");
+
 
 const getIdDbHandler = async (id) => {
     const data = await Dog.findAll({
         where: { id },
+        include: {
+            model: Temperament,
+            attributes: ["name"],
+            through: {
+                attributes: [],
+            },
+        },
     })
 
-    return data
+    const value = data.map(el => {
+        const temperaments = el.Temperaments.map((t) => t.name);
+        return {
+            id: el.id,
+            name: el.name,
+            image: el.image,
+            height: el.height,
+            weight: el.weight,
+            life_span: el.life_span,
+            temperament: temperaments.toString(", "),
+        }
+    })
+
+    return value
 }
 
 module.exports = getIdDbHandler;
