32d5c1bf12c46574e2e7a81c82a8dcf7095ce8a0
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-05-24 15:04:36 -0500

    cambio del dato temperament en la previsualizacion.

--- client/src/components/card/CardPreview.jsx
+++ client/src/components/card/CardPreview.jsx
@@ -6,17 +6,14 @@ import { useSelector } from "react-redux";
 function Card(props) {
     const temperamentos = useSelector(state => state.temperaments)
 
-
     const tempName = () => {
 
         const data = props.temperament.map(el => {
             return temperamentos.filter(ele => {
-                return ele.id === Number(el) && ele.name
+                return ele.id === Number(el) && ele.names
             })
         })
-
-        return data.flat(Infinity).map(el => el.name)
-
+        return data.flat(Infinity).map(el => el.names).join(", ");
     }
 
     const nombres = tempName();
@@ -26,11 +23,12 @@ function Card(props) {
     return (
         <div className={style.card}>
             <h2>{props.name}</h2>
-            <img className={style.image} src={props.image} alt="" />
+            { }
+            <img className={style.image} src={props.image} alt="" onError={(event) => { event.target.onerror = null; event.target.src = doggyNotFound}}/>
             <h3 className={style.subTitle}>Weight:</h3>
-            <p className={style.weight}>{props.weight} lb</p>
+            <p className={style.weight}>{props.weight} Kg</p>
             <h3 className={style.subTitle}>Temperaments:</h3>
-            <p className={style.temperament}>{nombres.toString()}</p>
+            <p className={style.temperament}>{nombres}</p>
             <h5 className={style.id}>#{props.id}</h5>
         </div>
     )
